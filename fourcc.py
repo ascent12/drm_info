d5113346112416b1d57e884122eedf4e7f59b525
Scott Moreau <oreaus@gmail.com>
2019-06-11 15:20:22 -0600

    Dynamically generate format lookup table from drm_fourcc header
    This eliminates the need to maintain a list of formats used
    for converting from fourcc codes to the prettier version by
    parsing drm_fourcc.h directly and building a header for use
    in pretty.c

---
+++ fourcc.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+fourcc_include = sys.argv[1]
+
+fmt_list = []
+mod_list = []
+
+fmt = re.compile(r'^#define (\w+)\s*(?:\\$\s*)?fourcc_code', flags=re.M)
+mod = re.compile(r'^#define (\w+)\s*(?:\\$\s*)?fourcc_mod_code', flags=re.M)
+mod_broadcom = re.compile(r'^#define (DRM_FORMAT_MOD_BROADCOM\w+(?=\s))', flags=re.M)
+
+with open(fourcc_include) as f:
+	for l in f.readlines():
+		ident = fmt.search(l)
+		if ident != None:
+			fmt_list.append(ident.group(1))
+			continue
+		ident = mod.search(l)
+		if ident != None:
+			mod_list.append(ident.group(1))
+			continue
+		ident = mod_broadcom.search(l)
+		if ident != None:
+			mod_list.append(ident.group(1))
+
+f_name = sys.argv[2]
+
+with open(f_name, 'w') as f:
+	f.write('''\
+#include <stdint.h>
+#include <drm_fourcc.h>
+
+static inline const char *format_str(uint32_t format)
+{
+	switch (format) {
+	case DRM_FORMAT_INVALID:
+		return "INVALID";
+''')
+	for ident in fmt_list:
+		f.write('\tcase {}:\n\t\treturn "{}";\n'.format(ident, ident[len('DRM_FORMAT_'):]))
+	f.write('''\
+	default:
+		return "unknown";
+	}
+}
+
+static inline const char *modifier_str(uint64_t modifier)
+{
+	/*
+	 * ARM has a complex format which we can't be bothered to parse.
+	 */
+	if ((modifier >> 56) == DRM_FORMAT_MOD_VENDOR_ARM) {
+		return "DRM_FORMAT_MOD_ARM_AFBC()";
+	}
+
+	switch (modifier) {
+''')
+	for ident in mod_list:
+		f.write('\tcase {}:\n\t\treturn "{}";\n'.format(ident, ident))
+	f.write('''\
+	default:
+		return "unknown";
+	}
+}
+''')
